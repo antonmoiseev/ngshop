e2b769647caa51537b817eea7f809483dbdba268
Anton Moiseev <a@antonmoiseev.com>
2016-12-25 18:39:57 +0400

    Home page improvements
    * Switch to MdTabGroup from MdTabNavBar
    * Dynamically provide categories
    * Listen to route params change
    * Set up redirects

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,7 +6,13 @@ import { CheckoutComponent } from './checkout/checkout.component';
 
 // TODO: Refactor to Routing Module in the advanced course.
 export const routes: Route[] = [
-  { path: '',         component: HomeComponent },
-  { path: 'product',  component: ProductComponent },
-  { path: 'checkout', component: CheckoutComponent }
+  { path: '', pathMatch: 'full', redirectTo: 'categories' },
+  { path: 'categories',
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'featured' },
+      { path: ':category', component: HomeComponent },
+    ]
+  },
+  { path: 'products', component: ProductComponent },
+  { path: 'checkout', component: CheckoutComponent },
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'ngs-home',
   styleUrls: [ './home.component.scss' ],
   templateUrl: './home.component.html'
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  // TODO: Make array items readonly after upgrading tp TypeScript 2.1
+  // https://blogs.msdn.microsoft.com/typescript/2016/12/07/announcing-typescript-2-1/#partial-readonly-record-and-pick
+  readonly categories = [
+    'feature',
+    'latest',
+    'fashion',
+    'furniture',
+    'beauty',
+    'food',
+    'travel'
+  ];
+
+  constructor(route: ActivatedRoute) {
+    // TODO: add switchMap()
+    route.params.subscribe(params => console.log(params));
+  }
+}
