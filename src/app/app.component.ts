9191250950d3d610760782d3a4c3e7d2f1b759a6
Anton Moiseev <a@antonmoiseev.com>
2016-12-25 14:56:18 +0400

    Add logo to the toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { MdIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'ngs-app',
   styleUrls: [ './app.component.scss' ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+      private iconRegistry: MdIconRegistry,
+      private domSanitizer: DomSanitizer) {
+    this.registerIcons(new Map<string, string>([
+      [ 'logo', 'assets/ngshop-logo.svg' ]
+    ]));
+  }
+
+  private registerIcons(icons: Map<string, string>) {
+    icons.forEach((url, id) => {
+      const safeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+      this.iconRegistry.addSvgIconInNamespace('ngs', id, safeUrl);
+    });
+  }
+}
