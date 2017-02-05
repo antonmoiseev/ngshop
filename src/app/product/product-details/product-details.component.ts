25ae227820aed18e71d3f1ebcc23b9d97464402b
Anton Moiseev <a@antonmoiseev.com>
2017-02-05 20:42:17 +0400

    Implement adding products to the shopping cart

--- src/app/product/product-details/product-details.component.ts
+++ src/app/product/product-details/product-details.component.ts
@@ -1,12 +1,21 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { Product } from '../../shared/services';
+import { Component, Input } from '@angular/core';
+import { Product, ShoppingCartService } from '../../shared/services';
 
 @Component({
   selector: 'ngs-product-details',
   styleUrls: [ './product-details.component.scss' ],
-  templateUrl: './product-details.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  templateUrl: './product-details.component.html'
 })
 export class ProductDetailsComponent {
+
   @Input() product: Product;
+
+  quantity: number;
+
+  constructor(private shoppingCartService: ShoppingCartService) {}
+
+  addItems() {
+    this.shoppingCartService.addItem(this.product.id, this.quantity);
+    this.quantity = null; // Reset selected number of items.
+  }
 }
