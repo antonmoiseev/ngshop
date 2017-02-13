d1c0d00261f341a203fbedf28e8b4827639d6727
Anton Moiseev <a@antonmoiseev.com>
2017-02-13 16:45:00 +0400

    Add initial checkout form UI

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   styleUrls: [ './checkout.component.scss' ],
   templateUrl: './checkout.component.html'
 })
-export class CheckoutComponent {}
+export class CheckoutComponent {
+
+  readonly months: string[] = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ];
+
+  readonly years: number[];
+
+  constructor() {
+    const currentYear = new Date().getFullYear();
+    this.years = Array.from({length: 10}, (_, index) => currentYear + index);
+  }
+
+}
